1f4a11d3e7dd63b9f2ce6ff00f159b920b51e0f7
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2022-01-26 12:34:59 -0300

    BuildMsiBundle

--- src/Installer/Installer.cs
+++ src/Installer/Installer.cs
@@ -10,47 +10,78 @@ using WixSharp.Controls;
 
 public class Installer
 {
-    const string APPDATA = "%AppDataFolder%";
-
-    const string projectName = "Teste";
-    const string outputName = "Teste";
-    const string outputDir = "output";
-    const string version = "1.0.0";
-
     private static void Main(string[] args)
     {
-        string installationDir = @$"{APPDATA}\Autodesk\ApplicationPlugins\";
+        string bundleFolder;
+        string projectName = "Installer";
+        string projectVersion = "1.0.0";
+        string outputInstaller = "Output";
+        Guid guid = new Guid("DDDDDDDD-DDDD-DDDD-DDDD-DDDDDDDDDDDD");
 
-        var fileName = new StringBuilder().Append(outputName).Append("-").Append(version);
-        //Additional suffixes for unique configurations add here
+        if (args.Length >= 1)
+        {
+            bundleFolder = args[0];
+            if (args.Length >= 2) projectName = args[1];
+            if (args.Length >= 3) projectVersion = args[2];
+            if (args.Length >= 4) outputInstaller = args[3];
+            projectVersion = BuildMsiBundle(bundleFolder, projectName, projectVersion, outputInstaller, guid);
+        }
+        else
+        {
+            Console.WriteLine("---------------------------------------------------------------------");
+            Console.WriteLine("Installer.exe bundleFolder projectName projectVersion outputInstaller");
+            Console.WriteLine("---------------------------------------------------------------------");
+        }
+    }
+
+    /// <summary>
+    /// Create Msi Installation
+    /// </summary>
+    /// <param name="bundleFolder"></param>
+    /// <param name="projectName"></param>
+    /// <param name="projectVersion"></param>
+    /// <param name="outputInstaller"></param>
+    /// <param name="guid"></param>
+    /// <returns></returns>
+    private static string BuildMsiBundle(string bundleFolder, string projectName, string projectVersion, string outputInstaller, Guid guid)
+    {
+        // Remove Revision 
+        projectVersion = Version.Parse(projectVersion).ToString(3);
+
+        var fileName = $"{projectName} {projectVersion}";
 
         var project = new Project
         {
             Name = projectName,
-            OutDir = outputDir,
+            OutDir = outputInstaller,
             Platform = Platform.x64,
             UI = WUI.WixUI_InstallDir,
-            Version = new Version(version),
+            Version = new Version(projectVersion),
             OutFileName = fileName.ToString(),
-            InstallScope = InstallScope.perUser,
+            InstallScope = InstallScope.perMachine,
             MajorUpgrade = MajorUpgrade.Default,
-            GUID = new Guid("DDDDDDDD-DDDD-DDDD-DDDD-DDDDDDDDDDDD"),
+            GUID = guid,
             //BannerImage = @"Installer\Resources\Icons\BannerImage.png",
             //BackgroundImage = @"Installer\Resources\Icons\BackgroundImage.png",
             ControlPanelInfo =
-            {
-                Manufacturer = Environment.UserName,
-                //ProductIcon = @"Installer\Resources\Icons\ShellIcon.ico"
-            },
-            Dirs = new Dir[] { CreateInstallDir(args[0]) }
+                {
+                    Manufacturer = Environment.UserName,
+                    //ProductIcon = @"Installer\Resources\Icons\ShellIcon.ico"
+                },
+            Dirs = new Dir[] { CreateInstallDir(bundleFolder) }
         };
-        project.RemoveDialogsBetween(NativeDialogs.WelcomeDlg, NativeDialogs.InstallDirDlg);
-        project.BuildMsi();
+        project.RemoveDialogsBetween(NativeDialogs.WelcomeDlg, NativeDialogs.VerifyReadyDlg);
+        return project.BuildMsi();
     }
 
+    /// <summary>
+    /// Create InstallDir
+    /// </summary>
+    /// <param name="bundleFolder"></param>
+    /// <returns></returns>
     private static InstallDir CreateInstallDir(string bundleFolder)
     {
-        var installationDir = @"%ProgramDataFolder%\Autodesk\ApplicationPlugins\";
+        var installationDir = @"%CommonAppDataFolder%\Autodesk\ApplicationPlugins\";
 
         var bundleFiles = new List<WixEntity>();
 
@@ -61,30 +92,6 @@ public class Installer
 
         return new InstallDir(installationDir, bundleFiles.ToArray());
     }
-
-    static WixEntity[] GenerateWixEntities(string[] args)
-    {
-        var versionStorages = new Dictionary<string, List<WixEntity>>();
-
-        foreach (var directory in args)
-        {
-            Console.WriteLine(directory);
-            var directoryInfo = new DirectoryInfo(directory);
-            var fileVersion = directoryInfo.Name;
-            var files = new Files($@"{directory}\*.*");
-            if (versionStorages.ContainsKey(fileVersion))
-                versionStorages[fileVersion].Add(files);
-            else
-                versionStorages.Add(fileVersion, new List<WixEntity> { files });
-
-            var assemblies = Directory.GetFiles(directory, "*", SearchOption.AllDirectories);
-            Console.WriteLine($"Added '{fileVersion}' version files: ");
-            foreach (var assembly in assemblies) Console.WriteLine($"'{assembly}'");
-        }
-
-        return versionStorages.Select(storage => new Dir(storage.Key, storage.Value.ToArray())).Cast<WixEntity>().ToArray();
-    }
-
 }
 
 
